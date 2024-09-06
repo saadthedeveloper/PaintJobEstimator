481e4af3a32db6a0bce5dad2761dad8e4254b5c8
saadthedeveloper <m.saad23145@gmail.com>
2024-09-06 15:10:35 +0500

    added comments, corrected logic

--- src/main/java/com/mycompany/paintjobestimator/PaintJobEstimator.java
+++ src/main/java/com/mycompany/paintjobestimator/PaintJobEstimator.java
@@ -6,8 +6,10 @@ package com.mycompany.paintjobestimator;
 import javax.swing.JOptionPane;
 
 /**
- *
- * @author MKAY
+ * This program estimates the cost of a paint job based on the number of rooms,
+ * wall space, paint cost, and labor required. It calculates the gallons of
+ * paint needed, hours of labor required, and the associated costs for paint and labor.
+ * The total cost of the paint job is then displayed.
  */
 public class PaintJobEstimator {
 
@@ -37,20 +39,24 @@ public class PaintJobEstimator {
 
         numberOfGallons = numberOfGallons(totalOfWallSpace);
         hoursOfLabor = hoursOfLabor(totalOfWallSpace);
-        paintCost = paintCost(totalOfWallSpace);
-        laborCharges = laborCharges(totalOfWallSpace);
+        paintCost = paintCost(numberOfGallons, priceOfPaintPerGallon);
+        laborCharges = laborCharges(hoursOfLabor);
         totalCost = totalCost(laborCharges, paintCost);
-        
-        System.out.println(numberOfGallons);
-        System.out.println(hoursOfLabor);
-        System.out.println(paintCost);
-        System.out.println(laborCharges);
-        System.out.println(totalCost);
+
+        JOptionPane.showMessageDialog(null, "Gallons of paint required: " + String.format("%.2f", numberOfGallons));
+        JOptionPane.showMessageDialog(null, "Hours of labor required: " + String.format("%.2f", hoursOfLabor));
+        JOptionPane.showMessageDialog(null, "Cost of paint: $" + String.format("%.2f", paintCost));
+        JOptionPane.showMessageDialog(null, "Labor charges: $" + String.format("%.2f", laborCharges));
+        JOptionPane.showMessageDialog(null, "Total cost of the paint job: $" + String.format("%.2f", totalCost));
 
     }
 
-    /*
-    
+    /**
+     * This method calculates the number of gallons of paint required.
+     *
+     * @param totalOfWallSpace The total wall space to be painted (in square
+     * feet).
+     * @return The number of gallons of paint required.
      */
     public static double numberOfGallons(double totalOfWallSpace) {
         double gallonsNeeded = totalOfWallSpace / 115;
@@ -58,43 +64,49 @@ public class PaintJobEstimator {
         return gallonsNeeded;
     }
 
+    /**
+     * This method calculates the total hours of labor required for the paint
+     * job.
+     *
+     * @param totalOfWallSpace The total wall space to be painted (in square
+     * feet).
+     * @return The total hours of labor required (based on 8 hours per 115
+     * square feet).
+     */
     public static double hoursOfLabor(double totalOfWallSpace) {
-        /*for square feet of wall to be covered 4.17 minutes will be required*/
-        int index = 1;
-        double hoursOfLabor = 0;
-        while (index <= totalOfWallSpace) {
-            hoursOfLabor += 4.17;
-            index++;
-        }
-        return hoursOfLabor;
+        return (totalOfWallSpace / 115) * 8;
     }
-    
-    public static double paintCost(double totalOfWallSpace) {
-        /*0.008695 gallons of paint is required for every squarefeet of wall to be covered*/
-        int index = 1;
-        double paintCost = 0;
-        while (index <= totalOfWallSpace) {
-            paintCost += 0.008695;
-            index++;
-        }
-        return paintCost;
+
+    /**
+     * This method calculates the total cost of paint.
+     *
+     * @param numberOfGallons The total number of gallons of paint required.
+     * @param pricePerGallon The price of a single gallon of paint.
+     * @return The total cost of paint for the job.
+     */
+    public static double paintCost(double numberOfGallons, double pricePerGallon) {
+        return numberOfGallons * pricePerGallon;
     }
-    
-    public static double laborCharges(double totalOfWallSpace) {
-        /*for every minute of work the labor cost is 0.0375*/
-        double laborCharges = 0;
-        double hoursOfLabor = hoursOfLabor(totalOfWallSpace);
-        int index = 1;
-         
-        while (index <=  hoursOfLabor) {
-            laborCharges += 0.0375;
-            index++;
-        }
-        return laborCharges;
+
+    /**
+     * This method calculates the total labor charges for the paint job.
+     *
+     * @param hoursOfLabor The total hours of labor required.
+     * @return The total labor charges (based on $18 per hour).
+     */
+    public static double laborCharges(double hoursOfLabor) {
+        double laborRate = 18.00;  // Labor rate is $18 per hour
+        return hoursOfLabor * laborRate;
     }
-    
-    public static double totalCost(double laborCharges, double paintCost) 
-    { 
+
+    /**
+     * This method calculates the total cost of the paint job (labor + paint).
+     *
+     * @param laborCharges The total labor charges.
+     * @param paintCost The total cost of paint.
+     * @return The total cost of the paint job.
+     */
+    public static double totalCost(double laborCharges, double paintCost) {
         double totalCost = 0;
         totalCost += (laborCharges + paintCost);
         return totalCost;
